1764e78a60e868fc3046e6bffc8a516c74d8b573
German Attanasio Ruiz <germanatt@us.ibm.com>
2014-12-04 17:04:52 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# User Modeling Ruby Sinatra Starter Application
+
+  The IBM Watson [User Modeling][user_modeling] service uses linguistic analysis to extract cognitive and social characteristics from input text such as email, text messages, tweets, forum posts, and more. By deriving cognitive and social preferences, the service helps users to understand, connect to, and communicate with other people on a more personalized level.
+
+## Getting Started
+
+1. Create a Bluemix Account
+
+  [Sign up][sign_up] in Bluemix, or use an existing account. Watson Services in Beta are free to use.
+
+2. Download and install the [Cloud-foundry CLI][cloud_foundry] tool
+
+3. Edit the `manifest.yml` file and change the `<application-name>` to something unique.
+  ```none
+  applications:
+  - services:
+    - um-service
+    name: <application-name>
+    path: .
+    memory: 256M
+  ```
+  The name you use will determinate your application url initially, e.g. `<application-name>.mybluemix.net`.
+
+4. Connect to Bluemix in the command line tool
+  ```sh
+  $ cf api https://api.ng.bluemix.net
+  $ cf login -u <your user ID>
+  ```
+
+5. Create the User Modeling service in Bluemix
+  ```sh
+  $ cf create-service user_modeling user_modeling_free_plan um-service
+  ```
+
+6. Push it live!
+  ```sh
+  $ cf push <application-name>
+  ```
+
+See the full [Getting Started][getting_started] documentation for more details, including code snippets and references.
+
+## License
+
+  This sample code is licensed under Apache 2.0. Full license text is available in [LICENSE](LICENSE).
+
+## Contributing
+
+  See [CONTRIBUTING](CONTRIBUTING.md).
+
+## Open Source @ IBM
+  Find more open source projects on the [IBM Github Page](http://ibm.github.io/)
+
+[user_modeling]: http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/systemuapi/
+[cloud_foundry]: https://github.com/cloudfoundry/cli
+[getting_started]: http://www.ibm.com/smarterplanet/us/en/ibmwatson/developercloud/doc/getting_started/
+[sign_up]: https://apps.admin.ibmcloud.com/manage/trial/bluemix.html?cm_mmc=WatsonDeveloperCloud-_-LandingSiteGetStarted-_-x-_-CreateAnAccountOnBluemixCLI
---
+++ app.rb
@@ -0,0 +1,161 @@
+#
+# Copyright IBM Corp. 2014
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'sinatra'
+require 'json'
+require 'excon'
+
+
+configure do
+  service_name = "user_modeling"
+
+  endpoint = Hash.new
+
+  set :endpoint, "<service_url>"
+  set :username, "<service_username>"
+  set :password, "<service_password>"
+
+  if ENV.key?("VCAP_SERVICES")
+    services = JSON.parse(ENV["VCAP_SERVICES"])
+    if !services[service_name].nil?
+      credentials = services[service_name].first["credentials"]
+      set :endpoint, credentials["url"]
+      set :username, credentials["username"]
+      set :password, credentials["password"]
+    else
+      puts "The service #{service_name} is not in the VCAP_SERVICES, did you forget to bind it?"
+    end
+  end
+
+  # Add a trailing slash, if it's not there
+  unless settings.endpoint.end_with?('/')
+    settings.endpoint = settings.endpoint + '/'
+  end
+
+  puts "endpoint = #{settings.endpoint}"
+  puts "username = #{settings.username}"
+end
+
+
+get '/' do
+  erb :index
+end
+
+
+post '/' do
+  @text = params[:content]
+
+  # build the request data with the text to analyze
+  content = {
+    :contentItems => [{
+      :userid => 'dummy',
+      :id => 'someid',
+      :sourceid => 'freetext',
+      :contenttype => 'text/plain',
+      :language => 'en',
+      :content => @text
+    }]
+  }
+
+  begin
+    profile = getProfile(content)
+
+    tree    = JSON.load(profile)["tree"]
+    @traits = flatten_systemu_traits(tree)
+
+    @viz = getVisualization(profile)
+
+  rescue Exception => e
+    @error = 'Error processing the request, please try again later.'
+    puts  e.message
+    puts  e.backtrace.join("\n")
+    return erb :index
+  end
+
+  erb :index
+end
+
+
+helpers do
+
+  # Create a job
+  def getProfile(data)
+    response = Excon.post(settings.endpoint + "api/v2/profile",
+                          :body => data.to_json,
+                          :headers => { "Content-Type" => "application/json" },
+                          :user => settings.username,
+                          :password => settings.password)
+
+    response.body
+  end
+
+  # Create a job
+  def getVisualization(data)
+    response = Excon.post(settings.endpoint + "api/v2/visualize?w=900&h=900&imgurl=/images/app.png",
+                          :body => data,
+                          :headers => { "Content-Type" => "application/json" },
+                          :user => settings.username,
+                          :password => settings.password)
+
+    response.body
+  end
+
+  # Some of the trait names can be misunderstood by lay people,
+  # make them a bit more approachable for the lay person
+  def NAME_SUBST
+    return {
+      "Anger"=> "Fiery",
+      "Anxiety"=> "Prone to worry",
+      "Depression"=> "Melancholy",
+      "Vulnerability"=> "Susceptible to stress",
+      "Liberalism"=> "Authority-challenging",
+      "Morality"=> "Uncompromising",
+      "Friendliness"=> "Outgoing",
+      "Neuroticism"=> "Emotional range"
+    }
+  end
+
+  def flatten_systemu_traits(traits)
+    arr = flatten_level(traits, 0)
+    return arr
+  end
+
+
+  # transform a traits tree in a list and format the value to: ##.#%
+  def flatten_level(t, level)
+    arr = []
+    if (level > 0 && ((not t.has_key?('children')) || level != 2))
+
+      obj = { 'id' => t['name'] }
+      if NAME_SUBST().has_key?(t['name'])
+        obj['id'] = NAME_SUBST()[t['name']]
+      end
+
+      obj['title'] = t.has_key?('children')
+      if t.has_key?('percentage')
+        obj['value'] = (t['percentage'] * 100).floor.to_s + "%"
+      end
+      arr.push(obj)
+    end
+    if t.has_key?('children') && t['id'] != "sbh"
+      t['children'].each do |child|
+        arr.push(flatten_level(child, level + 1))
+      end
+    end
+    return arr.flatten
+  end
+
+end
