77422e7b0be5e19fa9d4a91ad2ed8ab69a712a34
Ary Pablo Batista <batarypa@ar.ibm.com>
2015-07-31 08:04:21 -0300

    Added license to i18n config file

--- config/i18n.rb
+++ config/i18n.rb
@@ -1,13 +1,27 @@
+#
+# Copyright IBM Corp. 2014
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 require 'i18n'
 require 'i18n/backend/fallbacks'
-
 
 configure do
   I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
   I18n.load_path += Dir[File.join(settings.root, 'i18n', '*.yml')]
   I18n.backend.load_translations
 end
-
 
 def set_locale(request)
   puts "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
